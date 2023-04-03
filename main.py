da1ee361967cfafb2abf4fcfa86da856d17675a6
Caleb Han <calebhantech@gmail.com>
2023-04-03 15:59:28 -0400

    commit

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from decouple import config
 import praw
 import csv
+from textblob import TextBlob
+import matplotlib.pyplot as plt
+import datetime
+
+ID = config("ID")
+SECRET = config("SECRET")
 
 
 def get_data():
     keyword = input("KEYWORD TO SEARCH: ")
-    ID = config("ID")
-    SECRET = config("SECRET")
 
     reddit = praw.Reddit(
         client_id=ID,
@@ -18,7 +22,44 @@ def get_data():
         writer = csv.DictWriter(f, fieldnames=["Author", "Subreddit", "Date", "Title", "Text"])
         writer.writeheader()
         for submission in reddit.subreddit("all").search(query=keyword, sort="new", time_filter="month"):
-            row = {'Author': submission.author.name, "Subreddit": submission.subreddit.display_name, "Date": str(submission.created_utc), "Title": submission.title, "Text": submission.selftext}
+            row = {'Author': submission.author.name,
+                    "Subreddit": submission.subreddit.display_name,
+                    "Date": str(submission.created_utc),
+                    "Title": submission.title,
+                    "Text": submission.selftext
+            }
             writer.writerow(row)
 
-get_data()
+
+def compile_data():
+    with open("sentiment.csv", "w", encoding="utf-8", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=["Author", "Subreddit", "Date", "Title", "Text", "Polarity"])
+        writer.writeheader()
+        with open("data.csv", "r", encoding="utf-8", newline="") as f1:
+            reader = csv.DictReader(f1, delimiter=',')
+            for row in reader:
+                newRow = {'Author': row["Author"],
+                        "Subreddit": row["Subreddit"],
+                        "Date": row["Date"],
+                        "Title": row["Title"],
+                        "Text": row["Text"],
+                        "Polarity": TextBlob(row["Title"] + row["Text"]).sentiment.polarity
+                }
+                writer.writerow(newRow)
+
+
+def analyze_data():
+    with open("sentiment.csv", encoding="utf-8", newline="") as f:
+        reader = csv.DictReader(f, delimiter=',')
+        polarity = []
+        dates = []
+        for row in reader:
+            polarity.append(row["Polarity"])
+            dates.append(datetime.datetime.fromtimestamp(float(row["Date"])))
+        plt.plot(dates, polarity)
+        plt.xlabel('Time')
+        plt.ylabel('Polarity')
+        plt.show()
+
+
+analyze_data()
