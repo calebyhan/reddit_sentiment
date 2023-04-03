3e0b6089a9860963377bd55fa00e339191f29acc
Caleb Han <calebhantech@gmail.com>
2023-04-03 12:35:38 -0400

    commit

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ from decouple import config
 import praw
 import csv
 
-ID = config("ID")
-SECRET = config("SECRET")
 
-reddit = praw.Reddit(
-    client_id=ID,
-    client_secret=SECRET,
-    user_agent="Development stage",
-)
+def get_data():
+    keyword = input("KEYWORD TO SEARCH: ")
+    ID = config("ID")
+    SECRET = config("SECRET")
 
-with open("data.csv", "w", encoding="utf-8", newline="") as f:
-    writer = csv.DictWriter(f, fieldnames=["Author", "Subreddit", "Date", "Title"])
-    writer.writeheader()
-    for submission in reddit.subreddit("all").search(query="collegeboard", sort="new", time_filter="month"):
-        print(submission.name)
-        row = {'Author': submission.author.name, "Subreddit": submission.subreddit.display_name, "Date": str(submission.created_utc), "Title": submission.title}
-        writer.writerow(row)
+    reddit = praw.Reddit(
+        client_id=ID,
+        client_secret=SECRET,
+        user_agent="Development stage",
+    )
+
+    with open("data.csv", "w", encoding="utf-8", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=["Author", "Subreddit", "Date", "Title", "Text"])
+        writer.writeheader()
+        for submission in reddit.subreddit("all").search(query=keyword, sort="new", time_filter="month"):
+            row = {'Author': submission.author.name, "Subreddit": submission.subreddit.display_name, "Date": str(submission.created_utc), "Title": submission.title, "Text": submission.selftext}
+            writer.writerow(row)
+
+get_data()
